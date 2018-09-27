f544a96da72f8322b708364021af86984c53c448
domanisamuel <wafulasammie@gmail.com>
2018-09-27 11:00:12 +0300

    updated tests

--- app/tests/v1/tests_views.py
+++ app/tests/v1/tests_views.py
@@ -31,20 +31,3 @@ class OrderTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-# class OrderTest(unittest.Testcase):
-#     def test_order(self): #tests if the route can get a specific order
-#     response = app.test_client().get('/api/v1/order/1')
-#     assert response.status_code == 200
-
-#     def test_order_fail(self):
-#     response = app.test_client().get('/api/v1/order/')
-#     assert response.status_code == 404
-
-#     def test_orders(self): #tests if the route can get list of oreders
-#     response = app.test_client().get('/api/v1/orders')
-#     assert response.status_code == 200
-
-#     def test_edit_order(self):
-#     response = app.test_client().put('/api/v1/order/', data = {'status': 1}, content_type = 'application/json')
-#     assert response.ststus_code == 404 #returns 404 if order is not found
